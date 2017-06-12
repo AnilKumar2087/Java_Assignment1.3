4901b5a42a4d5e8ca6720a00c64e400d86f90860
AnilKumar2087 <a.anil.adunuri@gmail.com>
2017-06-12 22:40:36 +0530

    assignment

---
+++ src/Demo/Bit_Demo.java
@@ -0,0 +1,49 @@
+package Demo;
+
+import java.util.Scanner;
+
+public class Bit_Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		byte x;
+		byte y;
+		
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the values of 'x' and 'y' with '0' or '1'");
+		x = s.nextByte();
+		y = s.nextByte();
+		
+		if(x == 0 && y== 0){
+			System.out.println("LOGICAL AND value is '0'");	
+		}
+		else if(x == 0 && y == 1){
+			System.out.println("LOGICAL AND value is '0'");
+		}
+		else if(x == 1 && y == 0){
+			System.out.println("LOGICAL AND value is '0'");
+		}
+		else if(x == 1 && y == 1){
+			System.out.println("LOGICAL AND value is '1'");
+		}
+		
+		if(x == 0 & y== 0){
+			System.out.println("BITWISE AND value is '0'");	
+		}
+		else if(x == 0 & y == 1){
+			System.out.println("BITWISE AND value is '0'");
+		}
+		else if(x == 1 & y == 0){
+			System.out.println("BITWISE AND value is '0'");
+		}
+		else if(x == 1 & y == 1){
+			System.out.println("BITWISE AND value is '1'");
+		}
+		
+		
+
+	}
+
+}
